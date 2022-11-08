fa3845be3426a02523acaf7172d2f01fea147c17
stanislavBenet <stanislav.benet@gmail.com>
2022-11-08 12:11:18 +0200

    Done homework

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/* 
 Срок сдачи: Завтра
 1) Якщо значення змінної number позитивне або 0 - виводимо ''positive", інакше - "negative"
 
@@ -6,3 +6,29 @@
 
 3) Якщо значення змінної кратно(тобто ділеться без остачі) на 7 і одночасно кратна 3 - виводимо "you win!", інакше - "you loose"
 
+ */\
+
+//1
+const number = 3;
+if (number >= 0) {
+  console.log("positive");
+} else {
+  console.log("negative");
+}
+
+//2
+const num1 = 5;
+const num2 = 3;
+if (num1 > num2) {
+  console.log(num1 - num2);
+} else {
+  console.log(num1 * num2);
+}
+
+//3
+const number1 = 63;
+if (number1%7===0 && number1%3===0) {
+  console.log("you win");
+} else {
+  console.log("you lose");
+}
